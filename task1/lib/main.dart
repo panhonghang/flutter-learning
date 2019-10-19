b6fda34e8dcaa2345f6832fd6ecc23f26a59ed50
pangonghang <1966656576@qq.com>
2019-10-20 01:12:32 +0800

    常用组件练习

--- task1/lib/main.dart
+++ task1/lib/main.dart
@@ -79,7 +79,7 @@ class MyApp extends StatelessWidget{
           //   color: Colors.lightBlue,
           // ),
 
-          /* listview 组件使用 */
+          /* listview 竖直列表使用 组件使用 */
           // child: new ListView(
           //   padding: const EdgeInsets.all(50),
           //   children: <Widget>[
@@ -131,8 +131,81 @@ class MyApp extends StatelessWidget{
           //     ),
           //   ],
           // ),
+
+          /* 横向列表使用 listView 的 ScrollDirection*/
+          // child: new Container(
+          //   height: 400,
+          //   child: new ListView(
+          //     scrollDirection: Axis.horizontal,//设置为水平
+          //     children: <Widget>[
+          //       new Container(
+          //         width:180.0,
+          //         color: Colors.lightBlue,
+          //       ), new Container(
+          //         width:180.0,
+          //         color: Colors.amber,
+          //       ), new Container(
+          //         width:180.0,
+          //         color: Colors.deepOrange,
+          //       ),new Container(
+          //         width:180.0,
+          //         color: Colors.deepPurpleAccent,
+          //       ),
+          //     ],
+          //   ),
+          // ),
+
+          /* 使用提取的组件 */
+          // child: MyList(),
+
+          /* grid 组件 */
+          child: GridView(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              mainAxisSpacing: 2.0,
+              crossAxisSpacing: 2.0,
+              childAspectRatio: 0.7
+            ),
+            children: <Widget>[
+             new Image.network('http://img5.mtime.cn/mt/2018/10/22/104316.77318635_180X260X4.jpg',fit: BoxFit.cover),
+             new Image.network('http://img5.mtime.cn/mt/2018/10/10/112514.30587089_180X260X4.jpg',fit: BoxFit.cover),
+             new Image.network('http://img5.mtime.cn/mt/2018/11/13/093605.61422332_180X260X4.jpg',fit: BoxFit.cover),
+             new Image.network('http://img5.mtime.cn/mt/2018/11/07/092515.55805319_180X260X4.jpg',fit: BoxFit.cover),
+             new Image.network('http://img5.mtime.cn/mt/2018/11/21/090246.16772408_135X190X4.jpg',fit: BoxFit.cover),
+             new Image.network('http://img5.mtime.cn/mt/2018/11/17/162028.94879602_135X190X4.jpg',fit: BoxFit.cover),
+             new Image.network('http://img5.mtime.cn/mt/2018/11/19/165350.52237320_135X190X4.jpg',fit: BoxFit.cover),
+             new Image.network('http://img5.mtime.cn/mt/2018/11/16/115256.24365160_180X260X4.jpg',fit: BoxFit.cover),
+             new Image.network('http://img5.mtime.cn/mt/2018/11/20/141608.71613590_135X190X4.jpg',fit: BoxFit.cover),
+           
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+/* 提取组件 */
+// class MyList extends StatelessWidget{
+//   @override
+//   Widget build(BuildContext context){
+//     return ListView(
+//         scrollDirection: Axis.horizontal,
+//         children: <Widget>[
+//           new Container(
+//             width:180.0,
+//             color: Colors.lightBlue,
+//           ), new Container(
+//             width:180.0,
+//             color: Colors.amber,
+//           ), new Container(
+//             width:180.0,
+//             color: Colors.deepOrange,
+//           ),new Container(
+//             width:180.0,
+//             color: Colors.deepPurpleAccent,
+//           ),
+//         ],
+//     );
+//   }
+// }
