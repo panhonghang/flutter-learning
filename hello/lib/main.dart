9b4fd2f7aa7afe35262d052bc2a09c8908757ff3
pangonghang <1966656576@qq.com>
2019-10-21 22:52:48 +0800

    10-20

--- hello/lib/main.dart
+++ hello/lib/main.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
-// import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
 
+AudioPlayer audioPlayer = AudioPlayer();
+
+ play() async {
+      int result = await audioPlayer.play('http://music.163.com/song/media/outer/url?id=451703096.mp3');
+      if (result == 1) {
+        // success
+        print('play success');
+      } else {
+        print('play failed');
+      }
+    }
+
 Future<Post> fetchPost() async {
+  play();
   final response =
       await http.get('https://cnodejs.org/api/v1/topics');
   final responseJson = json.decode(response.body);
@@ -65,63 +78,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-// AudioPlayer audioPlayer = AudioPlayer();
-
-// class MyApp extends StatelessWidget {
-  
-//   @override
-//   Widget build(BuildContext context) {
-//     //音乐播放
-//     play() async{
-//         int result = await audioPlayer.play('http://music.163.com/song/media/outer/url?id=451703096.mp3');
-//       if (result == 1) {
-//         // success
-//         print('play success');
-//       } else {
-//         print('play failed');
-//       }
-//     }
-//     play();
-    
-//     return MaterialApp(
-//      title: 'demo',
-//       theme: ThemeData(
-//         primarySwatch: Colors.red,
-//       ),
-//       home: new Scaffold(
-//         appBar: new AppBar(
-//           title: new Text('welcome to flutter'),
-//         ),
-//         body: new Center(
-//           child: container,
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// //  var content;
-// //     // 接口请求
-// //     void getNet_2() async {
-// //     var client = http.Client();
-// //     http.Response response = await client.get('https://cnodejs.org/api/v1/topics');
-// //     content = response.body;
-// //     print(content);
-// //     }
-
-// // getNet_2();
-
-// var container = new Container( // grey box
-//   child: new Text(
-//     content,
-//     textAlign: TextAlign.center,
-//     style: new TextStyle(
-//       fontSize: 24.0,
-//       fontWeight: FontWeight.w900,
-//       fontFamily: "Georgia",
-//     ),
-//   ),
-//   width: 320.0,
-//   height: 240.0,
-//   color: Colors.red[100],
-// );
