5d653257e9367a10aaf3b96c1a977b8ae602449f
pangonghang <1966656576@qq.com>
2019-10-28 22:31:15 +0800

    架子搭建

--- task1/lib/pages/home_page.dart
+++ task1/lib/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import '../config/httpHeaders.dart';
+
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -7,6 +10,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   TextEditingController typeController = TextEditingController();
+  
   String showText = '欢迎来到美好人间';
 
   @override
@@ -14,32 +18,35 @@ class _HomePageState extends State<HomePage> {
     return Container(
       child: Scaffold(
         appBar: AppBar(title: Text('美好人间'),),
-        body: Container(
-          child: Column(
-            children: <Widget>[
-              TextField(
-                controller: typeController,
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(10.0),
-                  labelText: '美女类似',
-                  helperText: '输入类型'
+        // 键盘弹起改为滚动
+        body: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                TextField(
+                  controller: typeController,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(10.0),
+                    labelText: '美女类似',
+                    helperText: '输入类型'
+                  ),
+                  autofocus: false,
                 ),
-                autofocus: false,
-              ),
-              RaisedButton(
-                onPressed: (){_choiceAction();},
-                child: Text('选择完成'),
-              ),
-              Text(
-                // 变量字符
-                showText,
-                // 溢出折叠
-                overflow: TextOverflow.ellipsis,
-                // 最大行数
-                maxLines: 1,
-              )
-            ],
-          ),
+                RaisedButton(
+                  onPressed: (){_choiceAction();},
+                  child: Text('选择完成'),
+                ),
+                Text(
+                  // 变量字符
+                  showText,
+                  // 溢出折叠
+                  overflow: TextOverflow.ellipsis,
+                  // 最大行数
+                  maxLines: 1,
+                )
+              ],
+            ),
+          )
         ),
       ),
     );
@@ -66,7 +73,9 @@ class _HomePageState extends State<HomePage> {
     try{
       Response response;
       var data = {'limit':TypeText};
-      response = await Dio().get('http://10.24.191.242:4000/top/mv',
+      Dio dio = new Dio();
+      dio.options.headers = httpHeaders;
+      response = await dio.get('http://10.80.136.135:4000/top/mv',
         queryParameters: data
       );
       return response.data;
