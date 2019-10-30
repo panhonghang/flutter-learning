a554b0ce05476e4567a80644f3fcc502c716e4ba
pangonghang <1966656576@qq.com>
2019-10-31 00:51:15 +0800

    my_page

--- clould_music/lib/pages/my_page.dart
+++ clould_music/lib/pages/my_page.dart
@@ -18,15 +18,176 @@ class _MyPageState extends State<MyPage> {
            Container(
              height: 100,
              decoration: BoxDecoration(
-               color: Colors.red,
-              //  border: Border.all(,color: Colors.red)
+               border: Border(bottom: BorderSide(width: .5,color: Colors.grey[700]))
              ),
-             child: Row(
-               children: <Widget>[
-                 Text('cc')
-               ],
-             ),
-           )
+             child: ListView(
+                // This next line does the trick.
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  Container(
+                    width: 80.0,
+                    alignment: Alignment.center,
+                    child: Container(
+                      width: 60,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            height: 45,
+                            width: 45,
+                            margin: EdgeInsets.only(bottom: 10),
+                            child: Icon(Icons.ac_unit,color: Colors.white),
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.all(Radius.circular(60)),
+                            ),
+                          ),
+                          Text('私人FM',textScaleFactor: .8,style: TextStyle(color: Colors.grey[500]),)
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 80.0,
+                    alignment: Alignment.center,
+                    child: Container(
+                      width: 60,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            height: 45,
+                            width: 45,
+                            margin: EdgeInsets.only(bottom: 10),
+                            child: Icon(Icons.ac_unit,color: Colors.white),
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.all(Radius.circular(60)),
+                            ),
+                          ),
+                          Text('古典专区',textScaleFactor: .8,style: TextStyle(color: Colors.grey[500]),)
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 80.0,
+                    alignment: Alignment.center,
+                    child: Container(
+                      width: 60,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            height: 45,
+                            width: 45,
+                            margin: EdgeInsets.only(bottom: 10),
+                            child: Icon(Icons.ac_unit,color: Colors.white),
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.all(Radius.circular(60)),
+                            ),
+                          ),
+                          Text('私人FM',textScaleFactor: .8,style: TextStyle(color: Colors.grey[500]),)
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 80.0,
+                    alignment: Alignment.center,
+                    child: Container(
+                      width: 60,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            height: 45,
+                            width: 45,
+                            margin: EdgeInsets.only(bottom: 10),
+                            child: Icon(Icons.ac_unit,color: Colors.white),
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.all(Radius.circular(60)),
+                            ),
+                          ),
+                          Text('私人FM',textScaleFactor: .8,style: TextStyle(color: Colors.grey[500]),)
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 80.0,
+                    alignment: Alignment.center,
+                    child: Container(
+                      width: 60,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            height: 45,
+                            width: 45,
+                            margin: EdgeInsets.only(bottom: 10),
+                            child: Icon(Icons.ac_unit,color: Colors.white),
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.all(Radius.circular(60)),
+                            ),
+                          ),
+                          Text('私人FM',textScaleFactor: .8,style: TextStyle(color: Colors.grey[500]),)
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 80.0,
+                    alignment: Alignment.center,
+                    child: Container(
+                      width: 60,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            height: 45,
+                            width: 45,
+                            margin: EdgeInsets.only(bottom: 10),
+                            child: Icon(Icons.ac_unit,color: Colors.white),
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.all(Radius.circular(60)),
+                            ),
+                          ),
+                          Text('私人FM',textScaleFactor: .8,style: TextStyle(color: Colors.grey[500]),)
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 80.0,
+                    alignment: Alignment.center,
+                    child: Container(
+                      width: 60,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            height: 45,
+                            width: 45,
+                            margin: EdgeInsets.only(bottom: 10),
+                            child: Icon(Icons.ac_unit,color: Colors.white),
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.all(Radius.circular(60)),
+                            ),
+                          ),
+                          Text('私人FM',textScaleFactor: .8,style: TextStyle(color: Colors.grey[500]),)
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              )
+           ),
          ],
        ),
     );
